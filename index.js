e4dbf52c67c26808022e8fa5d1d863a3c63e9661
Lucas Silva <59655760+lucasb-silva@users.noreply.github.com>
2024-08-16 20:28:49 +0000

    Endpoint `\oi` adicionado & criação do script `start`

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello, world!")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+// Endpoint /oi -> Olá, mundo!
+app.get('/oi', function (req, res) {
+    res.send('Olá, mundo!')
+})
+
+app.listen(3000, function () {
+    console.log("Aplicação rodando em http://localhost:3000")
+})
